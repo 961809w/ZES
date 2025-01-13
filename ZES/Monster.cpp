d70912c4df5bf00c2c4db260e395fc52cca023dc
Choisehun3695 <choisehun3695@gmail.com>
2025-01-13 09:48:02 +0900

    아이템 드랍기능 추가

--- ZES/Monster.cpp
+++ ZES/Monster.cpp
@@ -1,73 +1,96 @@
 #include "Monster.h"
+#include "Item.h"
+#include "Character.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 //Monster class 구현
-monster::monster(const string& name, int health, int attack) : name(name), health(health), attack(attack) {}
+Monster::Monster(const string& name, int health, int attack) : name(name), health(health), attack(attack) {}
 
-monster::~monster() {}
+Monster::~Monster() {}
 
-string monster::getName() const {
+string Monster::getName() const {
 	return name;
 }
 
-int monster::getHealth() const {
+int Monster::getHealth() const {
 	return health;
 }
 
-int monster::getAttack() const {
+int Monster::getAttack() const {
 	return attack;
 }
 
-void monster::takeDamege(int damage) {
+void Monster::takeDamege(int damage) {
 	health -= damage;
 	if (health < 0) health = 0;
 }
 
-void monster::attackPlayer(Character& Character) {
+void Monster::attackPlayer(Character& Character) {
 	Character.takeDamage(attack);
 
 	cout << name << "attacks" << Character.getName() << "for" << attack << "damage!" << endl;
 }
 
-void monster::displayInfo() const {
+void Monster::displayInfo() const {
 	cout << "Monster: " << name << "Hralth: " << health << "Attack: " << attack << endl;
 }
 
+//아이템 드랍 설정
+unique_ptr<Item> Monster::dropitem() {
+	srand(static_cast<unsigned int>(time(nullptr)));
+	
+	int MoneyAmount = (rand() % 100) + 1;
+	cout << "처지 보상" << MoneyAmount << "골드" << endl;
+
+	int chance = rand() % 100; 
+	
+	if (chance < 30){
+		cout << "포션을 손에 넣었다!" << endl;
+		return make_unique<Potion>();
+	}
+
+return make_unique<Money>(MoneyAmount);
+}
+
+
 
 //watt 1500K class
-watt1500K::watt1500K() : monster("watt 1500K", 50, 10) {
+watt1500K::watt1500K() : Monster("watt 1500K", 10, 5) {
 }
 
 //watt 3000K class
-watt3000K::watt3000K() : monster("watt 3000K", 50, 10) {
+watt3000K::watt3000K() : Monster("watt 3000K", 30, 6) {
 }
 
 //watt 4000K class
-watt4000K::watt4000K() : monster("watt 4000K", 50, 10) {
+watt4000K::watt4000K() : Monster("watt 4000K", 50, 9) {
 }
 
 //watt 4500K class
-watt4500K::watt4500K() : monster("watt 4500K", 50, 10) {
+watt4500K::watt4500K() : Monster("watt 4500K", 55, 10) {
 }
 
 //watt 5000K class
-watt5000K::watt5000K() : monster("watt 5000K", 50, 10) {
+watt5000K::watt5000K() : Monster("watt 5000K", 60, 12) {
 }
 
 //watt 6000K class
-watt6000K::watt6000K() : monster("watt 4500K", 50, 10) {
+watt6000K::watt6000K() : Monster("watt 4500K", 65, 14) {
 }
 
 //watt 8000K class
-watt8000K::watt8000K() : monster("watt 8000K", 50, 10) {
+watt8000K::watt8000K() : Monster("watt 8000K", 80, 18) {
 }
 
 //watt 10000K class
-watt10000K::watt10000K() : monster("watt 10000K", 50, 10) {
+watt10000K::watt10000K() : Monster("watt 10000K", 100, 25) {
 }
 
 //watt 15000K class
-watt15000K::watt15000K() : monster("watt 15000K", 50, 10) {
+watt15000K::watt15000K() : Monster("watt 15000K", 300, 50) {
 }
+
--- ZES/Monster.h
+++ ZES/Monster.h
@@ -6,21 +6,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <memory>
 #include "Characeter.h"
+#include "Item.h"
 
 using namespace std;
 
 
 //moster class
-class monster {
+class Monster {
 protected:
 	string name;
 	int health;
 	int attack;
 
 public:
-	monster(const string& name, int health, int attack);
-	virtual ~monster();
+	Monster(const string& name, int health, int attack);
+	virtual ~Monster();
 
 	string getName() const;
 	int getHealth() const;
@@ -29,63 +32,66 @@ public:
 	virtual void takeDamege(int damage);
 	virtual void attackPlayer(Character& Character);
 	virtual void displayInfo() const;
+	unique_ptr<Item> dropitem();
 };
 
 
 //watt 1500K class
-class watt1500K : public monster {
+class watt1500K : public Monster {
 public:
 	watt1500K();
 };
 
 
 //watt 3000K class
-class watt3000K : public monster {
+class watt3000K : public Monster {
 public:
 	watt3000K();
 };
 
 
 //watt 4000K class
-class watt4000K : public monster {
+class watt4000K : public Monster {
 public:
 	watt4000K();
 };
 
 //watt 4500K class
-class watt4500K : public monster {
+class watt4500K : public Monster {
 public:
 	watt4500K();
 };
 
 //watt 5000K class
-class watt5000K : public monster {
+class watt5000K : public Monster {
 public:
 	watt5000K();
 };
 
 //watt 6000K class
-class watt6000K : public monster {
+class watt6000K : public Monster {
 public:
 	watt6000K();
 };
 
 //watt 8000K class
-class watt8000K : public monster {
+class watt8000K : public Monster {
 public:
 	watt8000K();
 };
 
 //watt 10000K class
-class watt10000K : public monster {
+class watt10000K : public Monster {
 public:
 	watt10000K();
 };
 
 //watt 15000K class
-class watt15000K : public monster {
+class watt15000K : public Monster {
 public:
 	watt15000K();
 };
+
+
 
 #endif
