c7dafa9844cd20b76b295ff14645dcc42415ea7b
961809w <961809w@gmail.com>
2025-01-13 19:07:28 +0900

    main 업데이트
    이지원

--- ZES/main.cpp
+++ ZES/main.cpp
@@ -1,18 +1,63 @@
-#include "Character.h"
-#include "Item.h"
 #include "GameManager.h"
-#include "Monster.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    Character player("ZEZE");
-    player.displayStatus();
+    GameManager gameManager;
 
-    player.addItem(std::make_shared<HealthPotion>(50));
-    player.addItem(std::make_shared<AttackBoost>(10));
+    // 게임 초기화
+    gameManager.initializeGame();
 
-    auto monster = GameManager::generateMonster(1);
-    GameManager::battle(player, monster);
+    // 플레이어 생성 (Singleton)
+    cout << "플레이어 이름을 입력하세요: ";
+    string playerName;
+    cin >> playerName;
+    Character* player = Character::getInstance(playerName);
 
-    player.displayStatus();
+    cout << "게임이 시작됩니다!" << endl;
+
+    bool isRunning = true;
+
+    while (isRunning) {
+        cout << "\n=========================" << endl;
+        cout << "1. 플레이어 상태 보기" << endl;
+        cout << "2. 인벤토리 보기" << endl;
+        cout << "3. 전투 시작" << endl;
+        cout << "4. 게임 종료" << endl;
+        cout << "=========================" << endl;
+
+        int choice;
+        cout << "선택: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            // 플레이어 상태 표시
+            gameManager.displayPlayerStatus(player);
+            break;
+
+        case 2:
+            // 인벤토리 표시
+            gameManager.displayInventory(player->inventory);
+            break;
+
+        case 3:
+            // 몬스터와 전투
+            gameManager.battle(player);
+            break;
+
+        case 4:
+            // 게임 종료
+            isRunning = false;
+            gameManager.endGame();
+            break;
+
+        default:
+            cout << "잘못된 입력입니다. 다시 선택하세요." << endl;
+            break;
+        }
+    }
+
     return 0;
 }
