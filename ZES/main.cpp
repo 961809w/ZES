4d09ee97b2ba3a07e8a657153445372a81eaf2ef
961809w <961809w@gmail.com>
2025-01-13 17:03:02 +0900

    필요없는 거 삭제, mian수정

--- ZES/main.cpp
+++ ZES/main.cpp
@@ -1,8 +1,18 @@
-//main.cpp
-
-#include <iostream>
+#include "Character.h"
+#include "Item.h"
+#include "GameManager.h"
+#include "Monster.h"
 
 int main() {
-    std::cout << "Hello, ZES Project!" << std::endl;
+    Character player("ZEZE");
+    player.displayStatus();
+
+    player.addItem(std::make_shared<HealthPotion>(50));
+    player.addItem(std::make_shared<AttackBoost>(10));
+
+    auto monster = GameManager::generateMonster(1);
+    GameManager::battle(player, monster);
+
+    player.displayStatus();
     return 0;
 }
