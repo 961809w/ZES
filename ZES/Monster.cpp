495d8004edff7e3c6c7d08f92d1f0f05ec7f25f4
Choisehun3695 <choisehun3695@gmail.com>
2025-01-14 11:19:57 +0900

    Update Monster.cpp
    오류 수정

--- ZES/Monster.cpp
+++ ZES/Monster.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-//Monster class ±¸Çö
+//Monster class êµ¬í˜„
 Monster::Monster(const string& name, int health, int attack, int experience) : name(name), health(health), attack(attack), experience(experience) {}
 
 Monster::~Monster() {}
@@ -34,81 +34,81 @@ void Monster::takeDamage(int damage) {
 }
 
 void Monster::attackPlayer(Character& character) {
-	character.takeDamage(attack);
+	character.maxHealth -= attack;
 
-	cout << name << "°¡" << character.getName() << "°ø°İÇß½À´Ï´Ù." << attack << "µ¥¹ÌÁö" << endl;
+	cout << name << "ê°€" << character.name << "ë¥¼ ê³µê²©í–ˆìŠµë‹ˆë‹¤." << attack << "ë°ë¯¸ì§€" << endl;
 }
 
 void Monster::displayInfo() const {
-	cout << "¸ó½ºÅÍ " << name << "Ã¼·Â " << health << "°ø°İ " << attack << endl;
+	cout << "ëª¬ìŠ¤í„° " << name << "ì²´ë ¥ " << health << "ê³µê²© " << attack << endl;
 }
 
 void Monster::grantexperienceToPlayer(Character& character) {
-	cout << name << " ¾²·¯Æ®·È´Ù " << character.getName() << " ¾ò¾ú½À´Ï´Ù. " << experience << " °æÇèÄ¡" << endl;
+	cout << name << "ë¥¼ ì“°ëŸ¬íŠ¸ë ¸ë‹¤ " << character.name << "ëŠ” ì–»ì—ˆìŠµë‹ˆë‹¤. " << experience << " ê²½í—˜ì¹˜ íšë“" << endl;
 	character.gainExperience(experience);
 }
 
-//¾ÆÀÌÅÛ µå¶ø ¼³Á¤
+//ì•„ì´í…œ ë“œë ì„¤ì •
 unique_ptr<Item> Monster::dropitem() {
 	srand(static_cast<unsigned int>(time(nullptr)));
 
 	int experience = (rand() % 100) + 10;
-	cout << "Ã³Áö º¸»ó" << experience << "°æÇèÄ¡" << endl;
+	cout << "ì²˜ì§€ ë³´ìƒ" << experience << "ê²½í—˜ì¹˜" << endl;
 
 	Character.gainExperience(EXP);
 
 	int gold = (rand() % 100) + 1;
-	cout << "Ã³Áö º¸»ó" << gold << "°ñµå" << endl;
+	cout << "ì²˜ì§€ ë³´ìƒ" << gold << "ê³¨ë“œ" << endl;
 
 	int chance = rand() % 100;
 
 	if (chance < 30) {
-		cout << "Æ÷¼ÇÀ» ÇÏ³ª¸¦ ¼Õ¿¡ ³Ö¾ú´Ù!" << endl;
+		cout << "í¬ì…˜ì„ í•˜ë‚˜ë¥¼ ì†ì— ë„£ì—ˆë‹¤!" << endl;
 		return make_unique<HealthPotion>();
 	}
-	if (chance < 30) && chance < 60){
-		cout << name << "¾îÅÃºÎ½ºÆ®¸¦ ¼Õ¿¡ ³Ö¾ú´Ù!" << endl;
+	if (chance < 30 && chance < 60){
+		cout << name << "ì–´íƒë¶€ìŠ¤íŠ¸ë¥¼ ì†ì— ë„£ì—ˆë‹¤!" << endl;
 		return make_unique<AttackBoost>();
 	}
 
-return make_unique<gold>(gold);
+return make_unique<Item>(gold);
 }
 
 
 
 //watt 1500K class
-watt1500K::watt1500K() : Monster("¿ÍÆ®·¹µå", 10, 5, 10) {
+watt1500K::watt1500K() : Monster("ë ˆë“œì™€íŠ¸", 10, 5, 10) {
 }
 
 //watt 3000K class
-watt3000K::watt3000K() : Monster("¿ÍÆ®·¹µå+", 30, 6, 15) {
+watt3000K::watt3000K() : Monster("ë ˆë“œì™€íŠ¸+", 30, 6, 15) {
 }
 
 //watt 4000K class
-watt4000K::watt4000K() : Monster("¿ÍÆ®¿¤·Î¿ì", 50, 9, 20) {
+watt4000K::watt4000K() : Monster("ì—˜ë¡œìš°ì™€íŠ¸", 50, 9, 20) {
 }
 
 //watt 4500K class
-watt4500K::watt4500K() : Monster("¿ÍÆ®¿¤·Î¿ì+", 55, 10, 25) {
+watt4500K::watt4500K() : Monster("ì—˜ë¡œìš°ì™€íŠ¸+", 55, 10, 25) {
 }
 
 //watt 5000K class
-watt5000K::watt5000K() : Monster("¿ÍÆ®±×¸°", 60, 12, 30) {
+watt5000K::watt5000K() : Monster("ê·¸ë¦°ì™€íŠ¸", 60, 12, 30) {
 }
 
 //watt 6000K class
-watt6000K::watt6000K() : Monster("¿ÍÆ®±×¸°+", 65, 14, 35) {
+watt6000K::watt6000K() : Monster("ê·¸ë¦°ì™€íŠ¸+", 65, 14, 35) {
 }
 
 //watt 8000K class
-watt8000K::watt8000K() : Monster("¿ÍÆ®ºí·ç", 80, 18, 40) {
+watt8000K::watt8000K() : Monster("ë¸”ë£¨ì™€íŠ¸", 80, 18, 40) {
 }
 
 //watt 10000K class
-watt10000K::watt10000K() : Monster("¿ÍÆ®ºí·ç+", 100, 25, 45) {
+watt10000K::watt10000K() : Monster("ë¸”ë£¨ì™€íŠ¸+", 100, 25, 45) {
 }
 
 //watt 15000K class
-watt15000K::watt15000K() : Monster("·¹ÀÎº¸¿ì¿ÍÆ®", 300, 50, 150) {
+watt15000K::watt15000K() : Monster("ë ˆì¸ë³´ìš°ì™€íŠ¸", 300, 50, 150) {
 }
 
